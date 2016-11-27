72210429e81e4edf96fda86da27d59fc82cf428a
QYJLMF <qyjlmf@hotmail.com>
2016-11-27 16:08:41 +0800

    golang

---
+++ client.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+var quitSemaphore chan bool
+
+func main() {
+	var tcpAddr *net.TCPAddr
+	tcpAddr, _ = net.ResolveTCPAddr("tcp", "172.21.92.122:9999")
+	conn, _ := net.DialTCP("tcp", nil, tcpAddr)
+	defer conn.Close()
+	fmt.Println("connected!")
+	go onMessageRecived(conn)
+	for {
+		var msg string
+		fmt.Scanln(&msg)
+		if msg == "quit" {
+			break
+		}
+		b := []byte(msg + "\n")
+		conn.Write(b)
+		//b := []byte("time\n")
+		//<-quitSemaphore
+	}
+}
+
+/*func onMessageRecived(conn *net.TCPConn) {
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		fmt.Println(msg)
+		if err != nil {
+			quitSemaphore <- true
+			break
+		}
+		time.Sleep(time.Second)
+		b := []byte(msg)
+		conn.Write(b)
+	}
+}*/
+func onMessageRecived(conn *net.TCPConn) {
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		fmt.Println(msg)
+		if err != nil {
+			quitSemaphore <- true
+			break
+		}
+	}
+}
