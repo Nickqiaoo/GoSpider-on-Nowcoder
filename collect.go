c9d677980a93e2150e4955b929a8479fe27f352a
Nickqiaoo <qyj970811@gmail.com>
2017-05-27 00:17:19 +0800

    保存到数据库

--- collect.go
+++ collect.go
@@ -1,27 +1,64 @@
 package nowcoder
 
-import ("regexp"
-"fmt"
-"os"
-"log")
-
-var idre ,_=regexp.Compile("-[0-9]\">.+")
-var notfound,_=regexp.Compile("<title>牛客网-用户不存在</title>")
+import (
+	"fmt"
+	"regexp"
+	//"os"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql" //sql
+	"log"
+	"sync"
+)
+//Wg var
+var Wg sync.WaitGroup
+var idre, _ = regexp.Compile("-[0-9]\">.+")
+var notfound, _ = regexp.Compile("<title>牛客网-用户不存在</title>")
+var cjre, _ = regexp.Compile("font-green\">[0-9]+")
+var db, err = sql.Open("mysql", "root:6055965@tcp(127.0.0.1:3306)/nowcoder?charset=utf8")
+var stmt, _ = db.Prepare(`INSERT per (uid,username,level,chengjiu) VALUES (?,?,?,?)`)
+var lock sync.Mutex 
 //Matchpage s
-func Matchpage(respose *[]byte) {
-	if notfound.Match(*respose){
-			fmt.Println("404")
-		}else {
-			id:=idre.Find(*respose)
-			fmt.Printf("%s",id)
-			file, err := os.OpenFile("nowcoder.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
-			num, err := file.Write(id)
-			if num != len(id) {
-				log.Fatal(err)
-			}	
+func Matchpage(respose *[]byte,i int) {
+	if notfound.Match(*respose) {
+		fmt.Println("404")
+	} else {
+		id := idre.Find(*respose)
+		if len(id)<1{
+			return
 		}
+		level := int(id[1]) - 48
+		id = id[4:]
+		chengjiu := cjre.Find(*respose)
+		chengjiu = chengjiu[12:]
+		sum := 0
+		for i := range chengjiu {
+			sum = sum*10 + (int(chengjiu[i]) - 48)
+		}
+		/*fmt.Printf("%s",id)
+		file, err := os.OpenFile("nowcoder.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		num, err := file.Write(id)
+		if num != len(id) {
+			log.Fatal(err)
+		}*/
+		stmt.Exec(i, id, level, sum)
+		fmt.Printf("%d %d %s %d\n",i,level,id,sum)
+	}
+	defer Wg.Done()
+}
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func convert(p []byte) string {
+	for i := 0; i < len(p); i++ {
+		if p[i] == 0 {
+			return string(p[0:i])
+		}
+	}
+	return string(p)
 }
--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	"net/http"
 	"nowcoder"
+	"os"
 	"strconv"
+	"time"
 )
-
-func main(){
-	url:="https://www.nowcoder.com/"
-	cli:=http.Client{}
-	for i:=0;i<1000;i++{	
-		response,_:=cli.Get(url+strconv.Itoa(i))
-		fmt.Println(response.Status+"\n")
-		body,_:=ioutil.ReadAll(response.Body)
-		nowcoder.Matchpage(&body)
-	}
-	db,err:=sql.Open("mysql", "root:6055965@/nowcoder")
-	err = db.Ping()
+func main() {
+	url := "https://www.nowcoder.com/"
+	cli := http.Client{}
+	file, err := os.OpenFile("nowcoder.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, os.ModePerm)
 	if err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
+	defer file.Close()
+	t := time.Now()
+	num, err := file.WriteString(t.String()+"\n")
+	//if num != len(t.String()) {
+		//log.Fatal(err)
+	//}
+	for i := 1; i < 200; i++ {
+		if i==404{
+			continue
+		}
+		response, _ := cli.Get(url + strconv.Itoa(i))
+		if response == nil{
+			continue
+		}
+		fmt.Println(response.Status)
+		body, _ := ioutil.ReadAll(response.Body)
+		if response.StatusCode == 200 {
+			nowcoder.Wg.Add(1)
+			nowcoder.Matchpage(&body,i)
+		}
+	}
+	t = time.Now()
+	num, err = file.WriteString(t.String())
+	if num != len(t.String()) {
+		log.Fatal(err)
+	}
+	nowcoder.Wg.Wait()
+
 }
